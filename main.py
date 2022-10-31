5bee1e4efc2c62e51660cc3af666c5d14f197597
thajime0125 <thajime0125@gmail.com>
2022-10-31 12:52:46 +0900

    add main

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+
+import pandas as pd
+import cv2
+
+from telop_recognition.telop_reading import reading_count
+
+FILE_DIR = "data/scores/"
+MOVIE_FILE = "data/kochi_gameonly.mp4"
+
+
+def main():
+    # Read movie file
+    cap = cv2.VideoCapture(MOVIE_FILE)
+
+
+    return
+
+if __name__ == '__main__':
+    main()
